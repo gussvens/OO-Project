dddaa0b909831ab245cb609183512cf50e4ae2a6
Marcus Pettersson <marcus.petterssonjo@gmail.com>
2016-03-23 20:16:58 +0100

    Added client class and made it able to communicate with servers

---
+++ src/main/java/edu/chalmers/projecttemplate/controller/ClientController.java
@@ -0,0 +1,57 @@
+package edu.chalmers.projecttemplate.controller;
+
+import java.io.IOException;
+import java.net.*;
+
+/**
+ * Created by Marcus on 2016-03-23.
+ */
+public class ClientController {
+
+    private DatagramSocket clientSocket;
+    private InetAddress IPAddress;
+    private byte[] sendData;
+    private byte[] receiveData;
+
+    public ClientController(byte[] address){
+
+        try {
+            clientSocket = new DatagramSocket();
+        } catch (SocketException s){
+            System.out.println("Something wrong with socket");
+        }
+
+        try {
+            IPAddress = InetAddress.getByAddress(address);
+        } catch (UnknownHostException u){
+            System.out.println("Unknown host");
+        }
+
+        sendData = new byte[1024];
+        receiveData = new byte[1024];
+
+        update();
+    }
+
+    private void update(){
+
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+
+        try {
+            clientSocket.send(sendPacket);
+        } catch (IOException e){
+            System.out.println("Couldn't send!");
+        }
+
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+        try{
+            clientSocket.receive(receivePacket);
+        } catch (IOException e){
+            System.out.println("Couldn't receive data!");
+        }
+
+
+    }
+
+}
