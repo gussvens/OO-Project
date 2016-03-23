29073609136246920df2f86680e8fbf99c0eb5ef
Erkan <sanneer@student.chalmers.se>
2016-03-23 20:26:47 +0100

    Added Server & ServerThread classes

---
+++ src/main/java/server/ServerThread.java
@@ -0,0 +1,35 @@
+package server;
+
+import java.io.*;
+import java.net.*;
+
+public class ServerThread extends Thread {
+	private Socket socket;
+
+	public ServerThread(Socket socket) throws SocketException{
+		super("ServerThread");
+		this.socket = socket;
+	}
+
+	public void run(){
+		try {
+			PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			output.println("Connected to client");
+			String inputString;
+			while ((inputString = input.readLine()) != null) {
+			
+				if (inputString.equals("/**Param*/")) {
+					//Do fancy stuff
+				}
+			}
+			
+			output.close();
+			input.close();
+			socket.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
