44b43af5c08737d79b00cefb2f2c1dedadc63ae4
Marcus Pettersson <marcus.petterssonjo@gmail.com>
2016-05-24 14:33:10 +0200

    Fixed bug when checking if players are dead

--- src/main/java/zombienado_v1/server/Server.java
+++ src/main/java/zombienado_v1/server/Server.java
@@ -22,6 +22,7 @@ public class Server extends Thread {
 	private int bulletCounter;
 	private int currentWave;
 	private int timeUntilNextWave;
+	private boolean gameOver = false;
 
 	private Server(){
 		players = new ArrayList<ServerPlayer>();
@@ -79,97 +80,100 @@ public class Server extends Thread {
 			/**
 			 * Logic
 			 */
-			for (int i = 0; i < players.size(); i++) {
-				final ServerThread st = serverThreads.get(i);
-				players.get(i).update(st.getDeltaX(), st.getDeltaY(), st.getDeltaRotation(),spawner.getZombies(),handler.getWallTiles());
-				if(st.getIsShooting()){
-					if(players.get(i).canShoot()) {
-						ArrayList<ServerBullet> temp = players.get(i).shoot(bulletCounter);
-						if(!temp.isEmpty()) {
-							for (int j = 0; j < temp.size(); j++) {
-								bullets.add(temp.get(j));
-								bulletCounter++;
+			if(!gameOver) {
+				for (int i = 0; i < players.size(); i++) {
+					final ServerThread st = serverThreads.get(i);
+					players.get(i).update(st.getDeltaX(), st.getDeltaY(), st.getDeltaRotation(), spawner.getZombies(), handler.getWallTiles());
+					if (st.getIsShooting()) {
+						if (players.get(i).canShoot()) {
+							ArrayList<ServerBullet> temp = players.get(i).shoot(bulletCounter);
+							if (!temp.isEmpty()) {
+								for (int j = 0; j < temp.size(); j++) {
+									bullets.add(temp.get(j));
+									bulletCounter++;
+								}
 							}
 						}
 					}
 				}
-			}
 
-			for (int i = bullets.size() - 1; i >= 0; i--){
-				bullets.get(i).update(spawner.getZombies(), handler.getWallTiles());
-				if (bullets.get(i).getSpeed() == 0){
-					bullets.remove(i);
-				}
-			}
-
-			/**
-			 * Send stuff to clients
-			 */
-			ArrayList<ServerPlayer> players = getPlayerPositions(); //tidy this up
-			spawner.update(this.players, handler.getSpawnTiles(),handler.getWallTiles());
-
-			if(spawner.getWave() != currentWave) {
-				currentWave = spawner.getWave();
-				for(ServerPlayer player : players){
-					if(player.getIsDead()){
-						player.setHealth(100);
-						player.setDead(false);
+				for (int i = bullets.size() - 1; i >= 0; i--) {
+					bullets.get(i).update(spawner.getZombies(), handler.getWallTiles());
+					if (bullets.get(i).getSpeed() == 0) {
+						bullets.remove(i);
 					}
 				}
-				for(ServerThread serverThread : serverThreads) {
-					serverThread.sendWaveData(currentWave);
-				}
-			}
 
-			if(spawner.getTimeUntilNextWave() != timeUntilNextWave) {
-				timeUntilNextWave = spawner.getTimeUntilNextWave();
+				/**
+				 * Send stuff to clients
+				 */
+				ArrayList<ServerPlayer> players = getPlayerPositions(); //tidy this up
+				spawner.update(this.players, handler.getSpawnTiles(), handler.getWallTiles());
+
+				if (spawner.getWave() != currentWave) {
+					currentWave = spawner.getWave();
+					for (ServerPlayer player : players) {
+						if (player.getIsDead()) {
+							player.setHealth(100);
+							player.setDead(false);
+						}
+					}
+					for (ServerThread serverThread : serverThreads) {
+						serverThread.sendWaveData(currentWave);
+					}
+				}
+
+				if (spawner.getTimeUntilNextWave() != timeUntilNextWave) {
+					timeUntilNextWave = spawner.getTimeUntilNextWave();
+
+					for (ServerThread serverThread : serverThreads) {
+						serverThread.sendTimeUntilNextWaveData(timeUntilNextWave);
+					}
+				}
 
 				for (ServerThread serverThread : serverThreads) {
-					serverThread.sendTimeUntilNextWaveData(timeUntilNextWave);
-				}
-			}
-
-			for (ServerThread serverThread : serverThreads){
-				if(serverThread.getWeaponHasChanged()) {
-					this.players.get(serverThread.getID()).switchWeapon(serverThread.getWeaponID());
-				}
-
-				for(int i = 0; i < players.size(); i++){
-					ServerPlayer player = players.get(i);
-					if(player.getIsDead()){
-						serverThread.sendDeadPlayerData(player.getID());
+					if (serverThread.getWeaponHasChanged()) {
+						this.players.get(serverThread.getID()).switchWeapon(serverThread.getWeaponID());
 					}
-					serverThread.sendPlayerData(i, player.getX(), player.getY(), player.getRotation(), player.hasShot(), player.getHealth(), player.getAmmo(), player.getBalance(), player.getWeaponID());
+
+					for (int i = 0; i < players.size(); i++) {
+						ServerPlayer player = players.get(i);
+						if (player.getIsDead()) {
+							serverThread.sendDeadPlayerData(player.getID());
+						}
+						serverThread.sendPlayerData(i, player.getX(), player.getY(), player.getRotation(), player.hasShot(), player.getHealth(), player.getAmmo(), player.getBalance(), player.getWeaponID());
+					}
+
+					for (ServerZombie zombie : spawner.getZombies()) {
+						serverThread.sendZombieData(spawner.getZombies().indexOf(zombie), zombie.getX(), zombie.getY(), zombie.getRotation());
+					}
+
+					for (ServerBullet bullet : bullets) {
+						serverThread.sendBulletData(bullets.indexOf(bullet), bullet.getX(), bullet.getY(), bullet.getRotation());
+
+					}
+
+
 				}
 
-				for(ServerZombie zombie : spawner.getZombies()){
-					serverThread.sendZombieData(spawner.getZombies().indexOf(zombie), zombie.getX(), zombie.getY(), zombie.getRotation());
+				int deadPlayers = 0;
+
+				for (int i = 0; i < players.size(); i++) {
+					ServerPlayer q = players.get(i);
+					q.resetShot();
+					if (q.getIsDead()) {
+						deadPlayers++;
+					}
 				}
 
-				for(ServerBullet bullet : bullets) {
-					serverThread.sendBulletData(bullets.indexOf(bullet), bullet.getX(), bullet.getY(), bullet.getRotation());
-
+				if(deadPlayers == players.size() && !players.isEmpty()) {
+					gameOver = true;
 				}
+			} else {
 
-
-			}
-
-			for(int i = 0; i < players.size(); i++){
-				ServerPlayer q = players.get(i);
-				q.resetShot();
-			}
-
-			int deadPlayers = 0;
-			for(int i = 0; i < players.size(); i++){
-				if(players.get(i).getIsDead()) {
-					deadPlayers++;
-				}
-			}
-			if(deadPlayers == players.size()){
 				for(ServerThread thread : serverThreads) {
 					thread.sendGameOver();
 				}
-				break;
 			}
 
 			System.out.println("SERVER: bullets: " + bullets.size());
