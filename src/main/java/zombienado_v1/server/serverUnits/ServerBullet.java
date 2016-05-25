18597dd3aef52e8db99deca8d9afc35694d7e89b
Marcus Pettersson <marcus.petterssonjo@gmail.com>
2016-05-25 19:05:17 +0200

    Added javadoc for ServerBullet

--- src/main/java/zombienado_v1/server/serverUnits/ServerBullet.java
+++ src/main/java/zombienado_v1/server/serverUnits/ServerBullet.java
@@ -19,6 +19,15 @@ public class ServerBullet implements iBullet {
     private int speed;
     private int damage;
 
+    /**
+     * Constructor for a Bullet
+     * @param x - The starting x-position of th eBullet
+     * @param y - The starting y-position of th eBullet
+     * @param ID - The ID of the Bullet
+     * @param rotation - The rotation of the Bullet
+     * @param damage - The damage of the Bullet
+     * @param bulletSpeed - The speed of the Bullet
+     */
     public ServerBullet(int x, int y, int ID, double rotation, int damage, int bulletSpeed){
         this.x = x;
         this.y = y;
@@ -26,31 +35,53 @@ public class ServerBullet implements iBullet {
         this.rotation = rotation;
         this.speed = bulletSpeed;
         this.damage = damage;
-        System.out.println("Shot fired!");
     }
 
+    /**
+     * @return - The x-position of the Bullet
+     */
     public int getX(){
         return x;
     }
 
+    /**
+     * @return - The y-position of the Bullet
+     */
     public int getY(){
         return y;
     }
 
+    /**
+     * @return - The rotation of the Bullet
+     */
     public double getRotation() { return rotation; }
 
+    /**
+     * @return - The ID of the Bullet
+     */
     public int getID(){
         return ID;
     }
 
+    /**
+     * @return - The speed of the Bullet
+     */
     public int getSpeed(){
         return speed;
     }
 
+    /**
+     * @return - The damage of the Bullet
+     */
     public int getDamage(){
         return damage;
     }
 
+    /**
+     * A method that updates the position of the bullet depending on it's rotation
+     * @param zombies - An ArraysList of all the ServerZombies on the map
+     * @param walls - An ArrayList of all the walls on the map
+     */
     public void update(ArrayList<ServerZombie> zombies, ArrayList<Point> walls){
 
         checkCollisionWithWalls(walls);
@@ -60,6 +91,10 @@ public class ServerBullet implements iBullet {
         y = y + (int)(speed*Math.sin(rotation));
     }
 
+    /**
+     * A method that checks for collision between a ServerZombie and a Bullet
+     * @param zombies - An ArrayList of ServerZombies
+     */
     private void checkZombieCollision(ArrayList<ServerZombie> zombies){
         for(ServerZombie zombie: zombies){
             if(new Circle(zombie.getX(),zombie.getY(),ServerZombie.getRadius()).contains(this.x,this.y)){
@@ -69,6 +104,10 @@ public class ServerBullet implements iBullet {
         }
     }
 
+    /**
+     * A method that checks for collision between walls and Bullets
+     * @param walls - An ArrayList of all the walls on the map
+     */
     private void checkCollisionWithWalls(ArrayList<Point> walls){
         int tileWidth = WorldHandler.getTileWidth();
         for(Point wall: walls){
