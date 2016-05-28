36bb9df01c68510fb1f8795c585179f1455dd512
Martin610 <martin610sson@hotmail.com>
2016-05-28 16:41:27 +0200

    Fixed filepathes

--- src/main/java/mapeditor_v1/Main.java
+++ src/main/java/mapeditor_v1/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("MapEditorStart.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/MapEditorStart.fxml"));
 
         Scene scene = new Scene(root);
         stage.setResizable(false);
